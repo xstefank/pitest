6910223570200a996e2f70d1b806c9cbfbf671af
xstefank <xstefank122@gmail.com>
2017-07-27 12:41:41 +0200

    transfer HigherOrderMutationDetails to interface

--- pitest-entry/src/main/java/org/pitest/mutationtest/build/HigherOrderMutationTestBuilder.java
+++ pitest-entry/src/main/java/org/pitest/mutationtest/build/HigherOrderMutationTestBuilder.java
@@ -8,11 +8,14 @@ import org.pitest.mutationtest.build.MutationTestBuilder;
 import org.pitest.mutationtest.build.NotGroupingGrouper;
 import org.pitest.mutationtest.build.WorkerFactory;
 import org.pitest.mutationtest.engine.MutationDetails;
+import org.pitest.mutationtest.engine.hom.AbstractHigherOrderMutationDetails;
 import org.pitest.mutationtest.engine.hom.HigherOrderMutationStrategy;
 import org.pitest.mutationtest.engine.hom.HigherOrderMutationStrategyDefinition;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -21,35 +24,59 @@ import java.util.List;
 public class HigherOrderMutationTestBuilder extends MutationTestBuilder {
 
     private String strategyId;
+    private MutationConfig mutationConfig;
 
-    public HigherOrderMutationTestBuilder(WorkerFactory workerFactory, MutationAnalyser analyser, MutationSource mutationSource, MutationGrouper grouper, String strategyId) {
+    public HigherOrderMutationTestBuilder(WorkerFactory workerFactory,
+                                          MutationAnalyser analyser,
+                                          MutationSource mutationSource,
+                                          MutationGrouper grouper,
+                                          MutationConfig mutationConfig,
+                                          String strategyId) {
         super(workerFactory, analyser, mutationSource, new NotGroupingGrouper());
+        this.mutationConfig = mutationConfig;
         this.strategyId = strategyId;
     }
 
     @Override
     public List<MutationAnalysisUnit> createMutationTestUnits(Collection<ClassName> codeClasses) {
         List<MutationAnalysisUnit> testUnits = super.createMutationTestUnits(codeClasses);
+        List<MutationDetails> mutations = collectMutations(testUnits);
+
+        HigherOrderMutationStrategy strategy = getStrategy(strategyId);
+        List<AbstractHigherOrderMutationDetails> higherOrderMutations = strategy.processMutations(mutations, mutationConfig);
+
+        Collections.sort(higherOrderMutations, comparator());
+
+        //TODO transfer to MutationAnalysisUnit
+
+        return testUnits;
+    }
+
+    private List<MutationDetails> collectMutations(List<MutationAnalysisUnit> testUnits) {
         List<MutationDetails> mutationDetails = new ArrayList<MutationDetails>();
 
         for (MutationAnalysisUnit testUnit : testUnits) {
             mutationDetails.addAll(testUnit.getMutations());
         }
-
-        //find the desired higher order strategy
-        HigherOrderMutationStrategy strategy = getStrategy(strategyId);
-
-        return testUnits;
+        return mutationDetails;
     }
 
     private HigherOrderMutationStrategy getStrategy(String strategyId) {
         HigherOrderMutationStrategyDefinition strategyDefinition = HigherOrderMutationStrategyDefinition.findById(strategyId);
 
         if (strategyDefinition == null) {
-            //unknown strategy requested
             throw new UnsupportedOperationException(String.format("Unknown higher order strategy [ %s ]", strategyId));
         }
 
         return strategyDefinition.getStrategy();
+    }
+
+    private Comparator<AbstractHigherOrderMutationDetails> comparator() {
+        return new Comparator<AbstractHigherOrderMutationDetails>() {
+            @Override
+            public int compare(AbstractHigherOrderMutationDetails o1, AbstractHigherOrderMutationDetails o2) {
+                return 0;
+            }
+        };
     }
 }
---
+++ pitest/src/main/java/org/pitest/mutationtest/engine/hom/AbstractHigherOrderMutationDetails.java
@@ -0,0 +1,49 @@
+package org.pitest.mutationtest.engine.hom;
+
+import org.pitest.functional.F2;
+import org.pitest.functional.FCollection;
+import org.pitest.mutationtest.engine.MutationDetails;
+import org.pitest.mutationtest.engine.MutationIdentifier;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ */
+public abstract class HigherOrderMutationDetails {
+
+    private List<MutationDetails> detailsList;
+    private int size;
+
+    public HigherOrderMutationDetails(List<MutationDetails> detailsList, int size) {
+        assert detailsList.size() == size;
+        this.size = size;
+        this.detailsList = new ArrayList<MutationDetails>(detailsList);
+    }
+
+    public List<MutationDetails> getDetailsList() {
+        return detailsList;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Collection<MutationIdentifier> getId() {
+        return FCollection.fold(detailsToId(), new ArrayList<MutationIdentifier>(), detailsList);
+    }
+
+    private static F2<Collection<MutationIdentifier>, MutationDetails, Collection<MutationIdentifier>> detailsToId() {
+
+        return new F2<Collection<MutationIdentifier>, MutationDetails, Collection<MutationIdentifier>>() {
+            @Override
+            public Collection<MutationIdentifier> apply(Collection<MutationIdentifier> mutationIdentifiers, MutationDetails details) {
+                mutationIdentifiers.add(details.getId());
+                return mutationIdentifiers;
+            }
+        };
+
+    }
+}
